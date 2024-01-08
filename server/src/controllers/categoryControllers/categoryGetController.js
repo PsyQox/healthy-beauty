30f1e0d30db3d7f88d894d237a9ed98e5ed2d758
PsyQox <luisrodrizza@gmail.com>
2024-01-07 23:43:57 -0600

    category search finalized

--- server/src/controllers/categoryControllers/categoryGetController.js
+++ server/src/controllers/categoryControllers/categoryGetController.js
@@ -1,8 +1,18 @@
 const { tbl_category } = require('../../db')
+const {Op} = require('sequelize')
 
-const categoryGetController = () => {
-    const categories = tbl_category.findAll()
-    return categories
+const categoryGetController = (categoryName) => {
+    if (categoryName) {
+        const categories = tbl_category.findAll({where:{
+            name:{
+                [Op.iLike]: `%${categoryName}%`
+            }
+        }})
+        return categories
+    }else{
+        const categories = tbl_category.findAll()
+        return categories
+    }
 }
 
 module.exports = categoryGetController
--- server/src/handlers/categoryHandlers/categoryGetHandler.js
+++ server/src/handlers/categoryHandlers/categoryGetHandler.js
@@ -2,9 +2,8 @@ const categoryGetController = require('../../controllers/categoryControllers/cat
 
 const categoryGetHandler = async (req,res) => {
     try {
-        // const {message} = req.params; 
-        // console.log(message);
-        const result = await categoryGetController() 
+        const { categoryName } = req.query; 
+        const result = await categoryGetController(categoryName) 
         res.status(200).json(result)
     } catch (error) {
         res.status(500).json({error: error.message})
