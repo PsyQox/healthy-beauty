89e2ae776034c7d18327f9fd95ca659dd282a28e
PsyQox <luisrodrizza@gmail.com>
2024-03-09 14:11:23 -0600

    category update

--- server/src/handlers/categoryHandlers/categoryPutHandler.js
+++ server/src/handlers/categoryHandlers/categoryPutHandler.js
@@ -5,7 +5,6 @@ const categoryPutHandler = async (req, res) => {
     const { id } = req.params
     const { image, name, description } = req.body
     if (!id.trim()) res.status(401).json({error: "ID not provided"}) 
-
     try {
         const response = await categoryUpdateController({id, image, name, description})
         if (response[0] === 1) {
