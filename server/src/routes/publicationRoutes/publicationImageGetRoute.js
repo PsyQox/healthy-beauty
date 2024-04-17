1997bce4df58b9e1e368754461718bb11062576d
PsyQox <luisrodrizza@gmail.com>
2024-04-17 10:57:12 -0600

    Agregue: Ruta Get del servidor para responder con imagenes guardadas de publicaciones

--- server/src/routes/publicationRoutes/publicationImageGetRoute.js
+++ server/src/routes/publicationRoutes/publicationImageGetRoute.js
@@ -1,9 +1,10 @@
 
 const { Router } = require('express')
+const publicationImageGetHandler = require('../../handlers/publicationHandlers/publicationImageGetHandler')
 
 const publicationImageGetRoute = Router()
 
-publicationImageGetRoute.get('publicationimage/:nameImage')
+publicationImageGetRoute.get('/publicationimage/:nameImage', publicationImageGetHandler)
 
 
 module.exports = publicationImageGetRoute
