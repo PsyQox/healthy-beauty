96ed54e565b3526e1117e92956e4e5b6e8615427
PsyQox <luisrodrizza@gmail.com>
2024-04-26 12:39:21 -0600

    modifique: Ruta PUT de categorias para eliminar la imagen de la base de datos cada que hay una modificacion de imagen

--- server/src/controllers/categoryControllers/categoryUpdateController.js
+++ server/src/controllers/categoryControllers/categoryUpdateController.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
+const fs = require('fs')
 const { tbl_category } = require('../../db')
 const { URL_SERVER } = process.env
 
-const categoryUpdateController = ({ id, image, name, description })=>{
+const categoryUpdateController = async ({ id, image, name, description })=>{
   if (image) {
+
+    const category = await tbl_category.findByPk(id)
+    const imageSplit = category.image.split('/')
+    const imageName = imageSplit[imageSplit.length - 1]
+    fs.unlinkSync(`./uploads/categoryImg/${imageName}`)
+
     const imageURL = `${URL_SERVER}/categoryimage/${image}`
     const response = tbl_category.update({ image:imageURL, name:name, description:description }, {
       where: {
