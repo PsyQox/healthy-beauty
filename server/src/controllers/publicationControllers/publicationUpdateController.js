f7a4c35418ded2b7463e4bab445f29f2eee69849
PsyQox <luisrodrizza@gmail.com>
2024-05-06 14:10:34 -0600

    Modifique: DELETE, PUT, de publicaciones, validando algunas cosas y retirando imagenes del servidor

--- server/src/controllers/publicationControllers/publicationUpdateController.js
+++ server/src/controllers/publicationControllers/publicationUpdateController.js
@@ -4,27 +4,30 @@ const { URL_SERVER } = process.env
 
 const publicationUpdateController = ({ id, title, files, description, tblUserId })=>{
 
-  if (files) {
-    const arrayImagesName = files?.map((file)=>{
-      const imageURL = `${URL_SERVER}/publicationimage/${file.filename}`
-      return imageURL
-    })
-    const response =  tbl_publication.update({ images:arrayImagesName, title:title, description:description, tblUserId: tblUserId}, {
-      where: {
-        id: id
-      }
-    });
-    return response
 
-  }else{
-    const response =  tbl_publication.update({ title:title, description:description, tblUserId: tblUserId}, {
-      where: {
-        id: id
-      }
-    });
-    return response
+    if(files.length > 0) {
+      const arrayImagesName = files?.map((file)=>{
+        const imageURL = `${URL_SERVER}/publicationimage/${file.filename}`
+        return imageURL
+      })
 
-  }
+      const response =  tbl_publication.update({ images:arrayImagesName, title:title, description:description, tblUserId: tblUserId}, {
+        where: {
+          id: id
+        }
+      });
+      return response
+    }else{
+      const response =  tbl_publication.update({ images:files, title:title, description:description, tblUserId: tblUserId}, {
+        where: {
+          id: id
+        }
+      });
+      return response
+    }
+    
+    
+
 }
 
 module.exports = publicationUpdateController
--- server/src/handlers/publicationHandlers/publicationPostHandler.js
+++ server/src/handlers/publicationHandlers/publicationPostHandler.js
@@ -5,8 +5,8 @@ const publicationPostHandler = async (req, res) => {
     const files = req.files
     const { title, description, tblUserId } = req.body;
     
-    if (!title.trim()  || !description.trim() || !tblUserId.trim()) return res.status(401).json({ error:'Missing data' })  
     try { 
+if (!title.trim()  || !description.trim() || !tblUserId.trim()) return res.status(401).json({ error:'Missing data' })  
        if (files.length !== 0) {
            const result = await publicationAddController({ title, files, description, tblUserId })
            res.status(201).json(result)
