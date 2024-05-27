b6bf27d6e13dcd33f17a66223ab666b689cab950
PsyQox <luisrodrizza@gmail.com>
2024-05-27 13:47:13 -0600

    Modifique: DELETE, PUT de services para aceptar imagenes e eliminarlas del servidor

--- server/src/controllers/serviceControllers/serviceUpdateController.js
+++ server/src/controllers/serviceControllers/serviceUpdateController.js
@@ -1,15 +1,44 @@
+const path = require('node:path')
+require('dotenv').config()
+const fs = require('fs')
 const { tbl_service } = require('../../db')
+const { URL_SERVER } = process.env
+const imageExists = require('../../utils/imageExists')
 
-
-const serviceUpdateController = ({ id, image, name, description, price, tblCategoryId })=>{
+const serviceUpdateController = async ({ id, image, name, description, price, tblCategoryId })=>{
   
-  const response =  tbl_service.update({ image:image, name:name, description:description, price:price, tblCategoryId: tblCategoryId}, {
-    where: {
-      id: id
+  if(image){
+    const service = await tbl_service.findByPk(id)
+    const imageSplit = service.image.split('/')
+    const imageName = imageSplit[imageSplit.length - 1]
+    const urlImage = path.join(__dirname, '../../../uploads/serviceImg',imageName)
+
+    const isExist = imageExists(urlImage)
+    
+    if (isExist) {
+        fs.unlinkSync(`./uploads/serviceImg/${imageName}`)
     }
-  });
-  
-  return response
+
+    const imageURL = `${URL_SERVER}/serviceimage/${image}`
+
+    const response =  tbl_service.update({image: imageURL,name:name, description:description, price:price, tblCategoryId: tblCategoryId}, {
+      where: {
+        id: id
+      }
+    });
+    
+    return response
+
+    
+  }else{
+    const response =  tbl_service.update({name:name, description:description, price:price, tblCategoryId: tblCategoryId}, {
+      where: {
+        id: id
+      }
+    });
+    
+    return response
+  }
 }
 
 module.exports = serviceUpdateController
--- server/src/routes/serviceRoutes/servicePutRoute.js
+++ server/src/routes/serviceRoutes/servicePutRoute.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const servicePutHandler = require('../../handlers/serviceHandlers/servicePutHandler')
-
+const uploadFile = require('../../middlewares/multerConfig')
 
 const servicePutRoute = Router()
 
-servicePutRoute.put("/updateservice/:id", servicePutHandler)
+const upload = uploadFile({dest: 'uploads/serviceImg/'})
+
+servicePutRoute.put("/updateservice/:id",upload.single('image'), servicePutHandler)
 
 module.exports = servicePutRoute
